1a42b63238eedfc422d8860ad8d7495ac85245aa
Dioniii <90280629+Dioniii@users.noreply.github.com>
2026-08-07 20:26:36 +0200

    tilemap import + first level design

--- gameplay.py
+++ gameplay.py
@@ -1,31 +1,20 @@
-from pgzero.builtins import Rect, keyboard
-
-GAME_WIDTH = 800
-PLAYER_START = (80, 275)
+import pgzrun
 
 
-player = Rect(PLAYER_START, (40, 50))
+TILE_SIZE = 18
+ROWS = 30
+COLS = 20
+
+WIDTH = ROWS * TILE_SIZE
+HEIGHT = COLS * TILE_SIZE
+TITLE = "Platformer Game"
 
 
-def start():
-    """Reset everything when a new game begins."""
-    player.topleft = PLAYER_START
-
+def draw():
+    pass
 
 def update():
-    """Update the game every frame."""
-    if keyboard.left:
-        player.x -= 4
-
-    if keyboard.right:
-        player.x += 4
-
-    player.x = max(0, min(player.x, GAME_WIDTH - player.width))
+    pass
 
 
-def draw(screen):
-    """Draw the gameplay screen."""
-    screen.fill((12, 25, 42))
-    screen.blit("background", (30, 17))
-    screen.draw.filled_rect(player, (204, 86, 45))
-    screen.draw.rect(player, (255, 245, 216))
+pgzrun.go()
